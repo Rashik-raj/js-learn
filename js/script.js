1a365af32e35e80bd2170967fe4a1f8c54bb8019
rashik-raj <rashik123.rs@gmail.com>
2021-09-05 20:55:16 +0545

    implement function

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function toFarenheit(celcius) {
+  const farenheit = celcius * (9 / 5) + 32;
+  console.log("Celcius: " + celcius);
+  console.log("Farenheit: " + farenheit);
+  return farenheit;
+}
